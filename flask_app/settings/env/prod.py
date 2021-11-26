f650a73364ea2bacd06ae7100c5d715529a33f79
Roberto Sanchez <rg.sanchez.a@gmail.com>
2021-11-26 10:29:44 -0500

    Accept Process

--- flask_app/settings/env/prod.py
+++ flask_app/settings/env/prod.py
@@ -7,5 +7,5 @@ prod["MONGO_LOG_LEVEL"] = {"value": "OFF", "options": ["ON", "OFF"]}
 prod["SECRET_KEY"] = "C45sdf4%&saduiotre56"
 prod["HOSTNAME"] = "10.30.200.119"
 prod["HOSTNAME_URL"] = "https://10.30.200.119"
-prod["from_email"] = "cenace_ce@cenace.org.ec"
+prod["from_email"] = "comitedeetica@cenace.org.ec"
 
